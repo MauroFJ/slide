7ba51f1080b1ac83466cdbf5b3cd90bcc3ac27ba
Mauro <zona.marques@gmail.com>
2024-09-30 15:54:33 -0300

    custom controls estilizados

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
-import { SlideNavegacao } from './slide.js'
+import SlideNavegacao from './slide.js'
 
 const slide = new SlideNavegacao('.slide','.slide-wrapper');
 slide.init();
-
-slide.addControl();
-
-slide.addArrow('.prev', '.next')
+slide.addArrow('.prev', '.next');
+slide.addControl('.custom-controls');
